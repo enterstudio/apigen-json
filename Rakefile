ac68e7af4b809496101e1fac3a9b2ad03162ca6d
fhur <fernando@authy.com>
2014-11-04 11:12:48 -0500

    Implemented json_generator

--- Rakefile
+++ Rakefile
@@ -17,8 +17,8 @@ Jeweler::Tasks.new do |gem|
   gem.name = "apigen-json"
   gem.homepage = "http://github.com/fhur/apigen-json"
   gem.license = "MIT"
-  gem.summary = %Q{TODO: one-line summary of your gem}
-  gem.description = %Q{TODO: longer description of your gem}
+  gem.summary = %Q{JSON Generator for Apigen}
+  gem.description = %Q{JSON Generator for Apigen}
   gem.email = "fernando@authy.com"
   gem.authors = ["fhur"]
   # dependencies defined in Gemfile
--- lib/apigen-json/json_generator.rb
+++ lib/apigen-json/json_generator.rb
@@ -1,14 +1,76 @@
 require 'apigen/generation/generator.rb'
 require 'mustache'
+require 'json'
 
 class JsonGenerator < Generator
 
   def generate(endpoint_group, opts={})
-    template = get_template
-    return Mustache.render(template, endpoint_group)
+    get_endpoint_group_hash(endpoint_group).to_json
   end
 
-  def get_template
-    File.read("#{__dir__}/template.mustache")
+  def get_endpoint_group_hash(endpoint_group)
+    endpoint_group.endpoints.map do |endpoint|
+      {
+        :name => endpoint.name,
+        :url => endpoint.url,
+        :method => {
+          :name => endpoint.method.name,
+          :has_body => endpoint.method.has_body
+        },
+        :request_type => endpoint.request_type,
+        :path_params => get_path_params_hash(endpoint.path_params),
+        :query_params => get_query_params_hash(endpoint.query_params),
+        :headers => get_headers_hash(endpoint.headers),
+        :request_params => get_request_params_hash(endpoint.request_params)
+      }
+    end
+  end
+
+  # converts a PathParam map to a Hash
+  def get_path_params_hash(path_params)
+    path_params.map do |path_param|
+      get_hash(header, [:name, :type])
+    end
+  end
+
+  # converts a QueryParam map to a Hash
+  def get_query_params_hash(query_params)
+    query_params.map do |query_param|
+      get_hash(header, [:name, :type])
+    end
+  end
+
+  # converts a Header map to a Hash
+  def get_headers_hash(headers)
+    headers.map do |header|
+      get_hash(header, [:name, :value])
+    end
+  end
+
+  # converts a RequestParam map to a Hash
+  def get_request_params_hash(request_params)
+    # TODO implement this
+    []
+  end
+
+  # given an object an a list of sym, this method
+  # return a hash mapping arg[i] to method call of obj.arg[i]
+  #
+  # Example args = [:a, :b, :method]
+  #
+  # The resulting hash will be
+  # {
+  #   :a => obj.a,
+  #   :b => obj.b,
+  #   :method => obj.method
+  # }
+  #
+  def get_hash(obj, args)
+    hash = {}
+    args.each do |arg|
+      val = obj.send(arg)
+      hash[arg] = val
+    end
+    return hash
   end
 end
